a6d5a1a5bc12213e837f857b3dcfb9ec8ea4796d
Agathe Vaisse <agathe.vaisse@gmail.com>
2018-11-06 11:06:57 +0100

    [BUILD] project initialized

---
+++ biscuits/biscuits/src/main/java/com/agathevaisse/biscuits/BiscuitsApplication.java
@@ -0,0 +1,12 @@
+package com.agathevaisse.biscuits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiscuitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiscuitsApplication.class, args);
+	}
+}
---
+++ biscuits/biscuits/src/test/java/com/agathevaisse/biscuits/BiscuitsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.agathevaisse.biscuits;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BiscuitsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
